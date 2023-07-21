0b495a272f81cc4c19f8aec565dd73019004f63a
Bugrahan TOKUYUCU <bugrahantokuyucu@192.168.1.5>
2023-07-21 09:51:05 +0300

    bug fix

--- work_orders/urls.py
+++ work_orders/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.work_order_list, name="work_order_list"),
     path("<str:filter>/", views.work_order_list, name="work_order_filtered_list"),
-    path("create/", views.work_order_create, name="work_order_create"),
+    path("create/new", views.work_order_create, name="work_order_create"),
     path("<int:work_order_pk>/details/", views.work_order_detail, name="work_order_detail"),
     path("<int:work_order_pk>/edit/", views.work_order_edit, name="work_order_edit"),
     path("<int:work_order_pk>/pdf/", views.create_pdf, name="create_work_order_pdf"),
